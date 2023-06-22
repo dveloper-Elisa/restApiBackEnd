a31c725f75a8266b06bf5bb8636808c6c2970245
IT Elisa Dev <114925665+dveloper-Elisa@users.noreply.github.com>
2023-06-22 09:59:21 +0200

    back end

---
+++ app.js
@@ -0,0 +1,22 @@
+const express = require("express");
+
+const app = express();
+app.use(express.json());
+// app.use(express.urlencoded({ extended: true }));   ==============> allowing form to be porsed
+// field of imports
+const authenticate = require("./userServices/authentication");
+
+// using app
+app.get("/", authenticate.Test);
+app.get("/api/users", authenticate.fetchData);
+app.post(
+  "/api/registeruser",
+  authenticate.emailVerify,
+  authenticate.registerUser
+);
+app.get("/api/resetassword", authenticate.sendResetPassword);
+// listening and ports
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+  return `@http://localhost:${PORT}`;
+});
